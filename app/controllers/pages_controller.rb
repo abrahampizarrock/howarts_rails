1511e85f348638515ff09cd94ffb829eb0b212d9
abrahampizarrock <128652700+abrahampizarrock@users.noreply.github.com>
2023-08-15 18:37:52 -0400

    botones y demaces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,20 @@ class PagesController < ApplicationController
   def index
     @characters = Character.all
   end
+  def buscar 
+    def buscar
+        def buscar
+          case params[:tipo_busqueda]
+          when 'casa'
+            @characters = Character.where(house: params[:casa])
+          when 'locacion'
+            @characters = Character.where(location: params[:locacion])
+          when 'nombre'
+            @characters = Character.where(name: params[:nombre])
+          else
+            @characters = Character.all
+          end
+        end        
+    end
+  end
 end
